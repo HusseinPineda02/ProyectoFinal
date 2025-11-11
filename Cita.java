5e261eaa17f44db42e68e78bb3ae8c800002be50
Joshiro19j <joshiroticomq@gmail.com>
2025-11-11 00:36:25 -0500

    Create Cita.java

---
+++ Cita.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+public class Cita {
+    private int idCita;
+    private Date fechaCita;
+    private String especialidad;
+    // Constructor 
+    public Cita(int idCita, Date fechaCita, String especialidad) {
+        this.idCita = idCita;
+        this.fechaCita = fechaCita;
+        this.especialidad = especialidad;
+    }
+    // Método princpal
+    public void mostrarDatosCita() {
+        System.out.println("Cita N°: " + idCita);
+        System.out.println("Fecha: " + fechaCita);
+        System.out.println("Especialidad: " + especialidad);
+    }
+
+    public void cancelarCita() {
+        System.out.println("La cita N° " + idCita + " ha sido cancelada.");
+    }
+}
