b0be04c692ce64bf390c01ee2421d9c01456925a
Hussein Pineda <hussein2pineda@gmail.com>
2025-11-10 22:02:01 -0500

    Clase Doctor y metodos

--- Doctor.java
+++ Doctor.java
@@ -1,3 +1,30 @@
-public class Doctor {
-    
+public class Doctor extends Trabajador {
+
+    ArrayList<Pacientes> p;
+    String especialidad;
+    Horario horario;
+
+    public Doctor() {
+
+    }
+
+    public void setEspecialidad(String especialidad){
+        this.especialidad = especialidad;
+    }
+
+    public void asignarCita() {
+
+    }
+
+    public String verHistorialPaciente(String dni){
+        int i = 0;
+        for(Paciente p : pacientes){
+            if(p.getDni == dni){
+                System.out.print(p.getHistorial());
+            }
+            i++;
+        }
+        return "PACIENTE NO ENCONTRADO\n";
+    }
+
 }
