2b0614661eb9f7b87a95d50e0020442f4fc1f48c
Joshiro19j <joshiroticomq@gmail.com>
2025-11-25 09:00:54 -0500

    Update Cita.java

--- Cita.java
+++ Cita.java
@@ -3,21 +3,30 @@ import java.util.Date;
 public class Cita {
     private int idCita;
     private Date fechaCita;
+    private String hora;
     private String especialidad;
-    // Constructor 
-    public Cita(int idCita, Date fechaCita, String especialidad) {
+    private Paciente paciente;
+    private Ambiente ambiente;
+
+    public Cita(int idCita, Date fechaCita, String hora, String especialidad, 
+                Paciente paciente, Ambiente ambiente) {
         this.idCita = idCita;
         this.fechaCita = fechaCita;
+        this.hora = hora;
         this.especialidad = especialidad;
+        this.paciente = paciente;
+        this.ambiente = ambiente;
+        ambiente.agregarCita(this); 
     }
-    // Método princpal
     public void mostrarDatosCita() {
-        System.out.println("Cita N°: " + idCita);
-        System.out.println("Fecha: " + fechaCita);
+        System.out.println("---CITA #" + idCita + "---");
+        System.out.println("Paciente: " + paciente.getNombreCompleto());
+        System.out.println("Fecha: " + fechaCita + " | Hora: " + hora);
         System.out.println("Especialidad: " + especialidad);
+        ambiente.mostrarInformacion();
     }
-
     public void cancelarCita() {
-        System.out.println("La cita N° " + idCita + " ha sido cancelada.");
+        ambiente.liberar();
+        System.out.println("Cita #" + idCita + " cancelada y ambiente liberado");
     }
 }
