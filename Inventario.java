d1f355de49a081d360f382e272af390bc9f804e6
Dayana306 <pradoalison74@gmail.com>
2025-11-25 11:27:06 -0500

    Add Inventario.java file

---
+++ Inventario.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class Inventario {
+
+    private HashMap<String, Integer> stock; // idMed → cantidad
+
+    public Inventario() {
+        stock = new HashMap<>();
+    }
+
+    public void agregarStock(String idMed, int cantidad) {
+        stock.put(idMed, stock.getOrDefault(idMed, 0) + cantidad);
+    }
+
+    public boolean reducirStock(String idMed, int cantidad) {
+        int actual = stock.getOrDefault(idMed, 0);
+        if (actual >= cantidad) {
+            stock.put(idMed, actual - cantidad);
+            return true;
+        }
+        return false;
+    }
+
+    public int consultarStock(String idMed) {
+        return stock.getOrDefault(idMed, 0);
+    }
+}
