5aaf88531e264100492fb50124339e6ecff71360
Joshiro19j <joshiroticomq@gmail.com>
2025-12-02 08:26:00 -0500

    Update Cita.java

--- Cita.java
+++ Cita.java
@@ -1,32 +1,21 @@
-import java.util.Date;
-
 public class Cita {
-    private int idCita;
-    private Date fechaCita;
-    private String hora;
-    private String especialidad;
-    private Paciente paciente;
-    private Ambiente ambiente;
+    private String paciente;
+    private String doctor;
+    private String fecha;
+    private String motivo;
 
-    public Cita(int idCita, Date fechaCita, String hora, String especialidad, 
-                Paciente paciente, Ambiente ambiente) {
-        this.idCita = idCita;
-        this.fechaCita = fechaCita;
-        this.hora = hora;
-        this.especialidad = especialidad;
+    public Cita(String paciente, String doctor, String fecha, String motivo) {
         this.paciente = paciente;
-        this.ambiente = ambiente;
-        ambiente.agregarCita(this); 
+        this.doctor = doctor;
+        this.fecha = fecha;
+        this.motivo = motivo;
     }
+
     public void mostrarDatosCita() {
-        System.out.println("---CITA #" + idCita + "---");
-        System.out.println("Paciente: " + paciente.getNombreCompleto());
-        System.out.println("Fecha: " + fechaCita + " | Hora: " + hora);
-        System.out.println("Especialidad: " + especialidad);
-        ambiente.mostrarInformacion();
-    }
-    public void cancelarCita() {
-        ambiente.liberar();
-        System.out.println("Cita #" + idCita + " cancelada y ambiente liberado");
+        System.out.println("---- DATOS DE LA CITA ----");
+        System.out.println("Paciente: " + paciente);
+        System.out.println("Doctor: " + doctor);
+        System.out.println("Fecha: " + fecha);
+        System.out.println("Motivo: " + motivo);
     }
 }
