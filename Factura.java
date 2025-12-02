b904a534069f08c0e66bcd0ef15b09c176d57b18
Dayana306 <pradoalison74@gmail.com>
2025-12-02 03:11:01 -0500

    Update Factura.java

--- Factura.java
+++ Factura.java
@@ -4,31 +4,94 @@ import java.util.ArrayList;
 public class Factura {
 
     private String idFactura;
+    private Paciente paciente;
+    private String tipoPago;
+    private double monto;
     private LocalDate fecha;
-    private String paciente;
-    private String medico;
-    private ArrayList<Medicamento> items;
-    private double total;
 
-    public Factura(String idFactura, String paciente, String medico) {
+    private ArrayList<Item> items; 
+
+    public Factura(String idFactura, Paciente paciente, String tipoPago, LocalDate fecha) {
         this.idFactura = idFactura;
-        this.fecha = LocalDate.now();
         this.paciente = paciente;
-        this.medico = medico;
+        this.tipoPago = tipoPago;
+        this.fecha = fecha;
         this.items = new ArrayList<>();
-        this.total = 0;
+        this.monto = 0;
     }
 
-    public void agregarItem(Medicamento m, double precio) {
-        items.add(m);
-        total += precio;
+
+    public String getIdFactura() {
+        return idFactura;
+    }
+    public Paciente getPaciente() {
+        return paciente;
+    }
+    public String getTipoPago() {
+        return tipoPago;
+    }
+    public double getMonto() {
+        return monto;
+    }
+    public LocalDate getFecha() {
+        return fecha;
+    }
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+    
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+    public void setIdFactura(String idFactura) {
+        this.idFactura = idFactura;
     }
 
-    public double getTotal() { return total; }
+    public void setTipoPago(String tipoPago) {
+        this.tipoPago = tipoPago;
+    }
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
+
+    public void agregarItem(Item item) {
+        items.add(item);
+        calcularTotal();
+    }
+
+    public void eliminarItem(String id) {
+        items.removeIf(i -> i.getIdItem().equals(id));
+        calcularTotal();
+    }
+
+    public void calcularTotal() {
+        this.monto = 0;
+        for (Item i : items) {
+            this.monto += i.getPrecio();
+        }
+    }
+
+
+    public void mostrarFactura() {
+        System.out.println("=========== FACTURA ===========");
+        System.out.println("ID Factura: " + idFactura);
+        System.out.println("Fecha: " + fecha);
+        System.out.println("Paciente: " + paciente.getNombreCompleto());
+        System.out.println("Tipo de Pago: " + tipoPago);
+        System.out.println("-------------- ITEMS --------------");
+        for (Item i : items) {
+            System.out.println(i);
+        }
+        System.out.println("----------------------------------");
+        System.out.println("TOTAL A PAGAR: S/ " + monto);
+        System.out.println("==================================");
+    }
 
     @Override
     public String toString() {
-        return "Factura{" + idFactura + ", total=" + total + "}";
+        return "Factura " + idFactura + " - " + fecha + " - Total: S/" + monto;
     }
 }
-
