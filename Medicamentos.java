e3a2f6cdddb53086d0df9c07abdfa2e5908d9d90
Dayana306 <pradoalison74@gmail.com>
2025-12-02 01:14:26 -0500

    Update Medicamentos.java

--- Medicamentos.java
+++ Medicamentos.java
@@ -1,26 +1,68 @@
-public class Medicamento {
+  public class Medicamento {
+
     private String idMed;
     private String nombre;
-    private String lote;
+    private int stock;
     private String descripcion;
     private String tipo;
 
-    public Medicamento(String idMed, String nombre, String lote, String descripcion, String tipo) {
+
+    public Medicamento(String idMed, String nombre, int stock, String descripcion, String tipo) {
         this.idMed = idMed;
         this.nombre = nombre;
-        this.lote = lote;
+        this.stock = stock;
         this.descripcion = descripcion;
         this.tipo = tipo;
     }
 
-    public String getIdMed() { return idMed; }
-    public String getNombre() { return nombre; }
-    public String getLote() { return lote; }
-    public String getDescripcion() { return descripcion; }
-    public String getTipo() { return tipo; }
+
+    public String getIdMed() { 
+        return idMed; 
+    }
+    public String getNombre() { 
+        return nombre; 
+    }
+    public int getStock() { 
+        return stock; 
+    }
+    public String getDescripcion() { 
+        return descripcion; 
+    }
+    public String getTipo() { 
+        return tipo; 
+    }
+
+
+    public void setIdMed(String idMed) { 
+        this.idMed = idMed; 
+    }
+    public void setNombre(String nombre) { 
+        this.nombre = nombre; 
+    }
+    public void setStock(int stock) { 
+        this.stock = stock; 
+    }
+    public void setDescripcion(String descripcion) { 
+        this.descripcion = descripcion; 
+    }
+    public void setTipo(String tipo) { 
+        this.tipo = tipo; 
+    }
+
+    public void reducirStock(int cantidad) {
+        if (cantidad <= stock) {
+            stock -= cantidad;
+        } else {
+            System.out.println("No hay suficiente stock para este medicamento.");
+        }
+    }
+
+    public boolean hayStock() {
+        return stock > 0;
+    }
 
     @Override
     public String toString() {
-        return "Medicamento{" + nombre + ", lote=" + lote + "}";
+        return idMed + " - " + nombre + " (" + tipo + ") Stock: " + stock;
     }
 }
